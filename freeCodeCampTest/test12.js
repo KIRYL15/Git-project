d9efea043f88c6c929d94d77ac77a5df44800fb2
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-10-18 22:28:27 +0300

    test12

---
+++ freeCodeCampTest/test12.js
@@ -0,0 +1,2 @@
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/generate-random-whole-numbers-with-javascript
+let randomWholeNum = () => Math.floor(Math.random()*10)
