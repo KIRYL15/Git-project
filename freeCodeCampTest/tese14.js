14e141442764fdccc4f84e924a6bb2b7f3665c3e
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-10-18 23:04:19 +0300

    test14

---
+++ freeCodeCampTest/tese14.js
@@ -0,0 +1,4 @@
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-the-parseint-function
+let convertToInteger=(str) => parseInt(str) 
+
+convertToInteger("56");
