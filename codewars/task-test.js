849dcbdf1873ec0a45578ea7597edf717b6ec033
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-10-18 22:23:37 +0300

    test11

---
+++ codewars/task-test.js
@@ -0,0 +1,8 @@
+//пример для разбора
+function isInteger(num) {
+    return (num ^ 0) === num;
+  }
+  
+  alert( isInteger(1) ); // true
+  alert( isInteger(1.5) ); // false
+  alert( isInteger(-0.5) ); // false
---
+++ freeCodeCampTest/test11.js
@@ -0,0 +1,4 @@
+//измеить функцию для вовзрата случайного числа вместо 0
+function randomFraction() {
+   return Math.random() * 10;
+}
