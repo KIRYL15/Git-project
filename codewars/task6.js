67019818d23e2b31e54519502fababa39f2c3b55
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-10-19 20:32:34 +0300

    task6

---
+++ codewars/task6.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/56b1f01c247c01db92000076/train/javascript
+function doubleChar(str) {
+    let sum =[];
+    for (let i=0; i<str.length; i++){
+      sum +=str [i]+=str[i]
+      
+    }return sum
+  }
