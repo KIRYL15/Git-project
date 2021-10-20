1b62b7836dee88882d6993d64a36a0e84a1d0ece
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-10-20 17:24:29 +0300

    task16

---
+++ codewars/task16.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
+function removeChar(str){
+    let newremoveChar = str.substring(1, str.length-1)
+    
+     return newremoveChar
+      
+    };
+    
+    
+    
+    
