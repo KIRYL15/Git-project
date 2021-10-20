66dec37205069fe39f24dc59fbb6db6531966500
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-10-20 17:13:45 +0300

    task15

---
+++ codewars/task15.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
+function maps(x){
+    let doubled = [];
+      for (let i = 0; i < x.length; i++) {
+          doubled.push(x[i] * 2);
+      }
+      return doubled;
+  }
+   
+  
