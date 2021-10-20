5c38a433bf1c85890f77e9d03a0c4c847d5c0352
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-10-20 21:12:17 +0300

    task20

---
+++ codewars/task20.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/551b4501ac0447318f0009cd/solutions/javascript
+function booleanToString(b){
+    let yes="true"
+    let no = "false"
+    if (b === true){
+      return yes
+    } else {
+      return no
+    }
+    
+    
+    
+    
+    
+  }
