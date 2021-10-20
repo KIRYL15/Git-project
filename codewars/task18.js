703d62c26ca5fb6e3f15c5eda88d62039688c89e
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-10-20 20:20:37 +0300

    task18

---
+++ codewars/task18.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/51c8991dee245d7ddf00000e/train/javascript
+function reverseWords(str){
+    let  reversed= str.split(' ').reverse().join(' ');
+    return reversed
+  }
