811b815162d56ee01d7d74e39e31eb049a8ad108
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-10-20 16:45:15 +0300

    task12

---
+++ codewars/task12.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+function boolToWord(bool){
+    let ye = "Yes";
+     let no="No"
+     
+     if (bool == true) {
+       return ye
+       } else {
+         return no
+     }
+   }
