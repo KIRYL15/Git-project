d08379a1fe95a7987fc329aeafa1c651ac540738
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-10-21 20:49:26 +0300

    task29

---
+++ codewars/task29.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+var min = function(list){
+    let num1 = Math.min(...list)
+    return num1;
+}
+
+var max = function(list){
+   let num2 = Math.max(...list)
+   return num2;
+}
