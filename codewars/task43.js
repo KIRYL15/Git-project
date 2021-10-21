ad96bd1733abd2bc26d96c64574a2e4e6a855748
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-10-22 00:26:05 +0300

    task43

---
+++ codewars/task43.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/5ab6538b379d20ad880000ab/train/javascript
+const areaOrPerimeter = function(l , w) {
+    let num = l * w
+  let num2 = (l+w) * 2
+    if(l===w){
+      return num
+    }else {
+      return num2
+    }
+    
+    
+    
+  };
