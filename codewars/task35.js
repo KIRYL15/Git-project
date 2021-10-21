64b2ece25efa233581b47f817f519aa1e33f579a
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-10-21 23:03:52 +0300

    task35

---
+++ codewars/task35.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/javascript
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    let sum =   laLigaGoals + copaDelReyGoals + championsLeagueGoals
+    
+    return sum
+    // code goes here
+  }
