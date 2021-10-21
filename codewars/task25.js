0976b16da0bfc0fc92b7ae79f51b8a049f4b0859
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-10-21 19:45:48 +0300

    task25

---
+++ codewars/task25.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/javascript
+//перевод строки в массив
+function stringToArray(string){
+
+	let mas = string.split(" ")
+return mas
+
+}
