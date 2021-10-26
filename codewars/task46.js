31fe8963bfc9307e3fc02f6c5ddc9de70e34d04c
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-10-27 00:48:21 +0300

    task46

---
+++ codewars/task46.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/5977618080ef220766000022/train/javascript
+function usdcny(usd) {
+    let chu = 6.75 * usd;
+    let newStr = chu.toFixed(2)
+    let str = newStr + " Chinese Yuan"
+  return str
+  }
