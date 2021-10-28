8f3090aa1f63e3d1f3809d1467fcde01ddc66d32
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-10-29 00:22:37 +0300

    task50

---
+++ codewars/task50.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
+function simpleMultiplication(number) {
+    if (number%2===0){
+      return number*8
+    }else{
+      return number*9
+    }
+}
+function simpleMultiplication(n) {
+    return n * (n % 2 ? 9 : 8);
+}
+function simpleMultiplication(n){
+    return n % 2 == 0 ? n * 8 : n * 9
+  }
+  const simpleMultiplication=n=>n*(n%2?9:8)
