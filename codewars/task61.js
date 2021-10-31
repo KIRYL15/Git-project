b0068fecc3a1513679499f39839924247b9c9d41
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-10-31 22:32:10 +0300

    task61

---
+++ codewars/task61.js
@@ -0,0 +1,17 @@
+//https://www.codewars.com/kata/5a360620f28b82a711000047/train/javascript
+function defineSuit(card) {
+    let club=card.indexOf('♣')
+    let dimond=card.indexOf('♦')
+    let heart=card.indexOf('♥')
+    let spa=card.indexOf('♠')
+    
+       if(club===1 || club===2){
+        return 'clubs'
+    }else if(dimond===1 || dimond===2){
+      return 'diamonds'
+    }else if(heart===1 || heart===2){
+          return 'hearts'
+    }else if(spa===1 || spa===2){
+          return 'spades'
+    }
+     }
